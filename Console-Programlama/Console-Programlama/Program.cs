376bd5c67986b0731aee93016cd487ae3b5a3b09
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2021-11-28 16:32:10 +0300

    Add files via upload

---
+++ Console-Programlama/Console-Programlama/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Console_Programlama
+{
+    class Program
+    {
+        static void Main(string[] args)  //default olarak public'tir
+        {
+            Console.WriteLine("Hello World!");//console bir sınıf
+            Console.WriteLine("İsminizi girin:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi girin:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + lastName);
+        }
+    }
+}
+
+//ilk çalışcak olan satır main'in ilk satırı
+//son çalışacak olan satır main'in son satırı
+
+//static:nesne yaratılmadan methoda ulaşılabileceğini belirtir
+
+//string[] args: komut satırı argümanlarını almak için
