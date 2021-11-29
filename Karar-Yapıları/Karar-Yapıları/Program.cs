387489f82be738952299a869898b8ce65dea211e
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2021-11-29 21:42:01 +0300

    Add files via upload

---
+++ Karar-Yapıları/Karar-Yapıları/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Karar_Yapıları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if(time>=6 && time <= 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi geceler");
+            }
+            //ternary if
+            string sonuc = time < 18 ? "İyi günler" : "İyi geceler";
+            
+            sonuc = time >= 6 && time <= 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+
+
+            //switch case
+            int month = DateTime.Now.Month;
+
+            //expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat");
+                    break;
+                case 11:
+                    Console.WriteLine("Kasım");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girişi");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış Mevsimindesiniz");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar mevsimindesiniz");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz mevsimindesiniz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar mevsimindesiniz");
+                    break;
+            }
+
+
+
+
+
+        }
+    }
+}
