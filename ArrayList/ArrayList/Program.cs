f9dff5d2369676b9f175b42ac434f6ba8ffd5bb0
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2022-02-24 13:20:15 +0300

    Add files via upload

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Birden fazla veri tipi saklanabilir.
+           
+            ArrayList liste = new ArrayList();
+            liste.Add("Ayse");
+            liste.Add(2);
+            liste.Add(true);
+            liste.Add('A');
+
+            //içerisindeki verilere erişim
+            Console.WriteLine(liste[1]);
+            foreach(var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //AddRange
+            Console.WriteLine("*** Add Range ***");
+            string[] renkler = { "kirmizi", "sari","yesil"};
+            List<int> sayilar = new List<int>{1,8,3,7,9,92,5};
+            liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sort
+            Console.WriteLine("*** Sort ****");
+
+            //Binary Search
+            Console.WriteLine("*** Binary Search ***");
+            Console.WriteLine(liste.BinarySearch(9)); //indexini döndürür
+
+
+            //Reverse
+            Console.WriteLine("*** Reverse ***");
+            liste.Reverse();
+            foreach(var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Clear
+            Console.WriteLine("*** Clear ****");
+            liste.Clear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
