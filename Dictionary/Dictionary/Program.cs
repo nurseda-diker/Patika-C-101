0dfac3a8886f2212c7466c0c318a61be8e396e1f
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2022-03-01 18:13:41 +0300

    Add files via upload

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, string> kullanicilar = new Dictionary<int,string>();
+
+            kullanicilar.Add(10, "Ayse Yilmaz");
+            kullanicilar.Add(12, "Ahmet Yilmaz");
+            kullanicilar.Add(18, "Deniz Arda");
+            kullanicilar.Add(20, "Özcan Cosar");
+
+            //Dizinin elemanlarına erişim
+            Console.WriteLine("*** Elemanlara erişim ***");
+            Console.WriteLine(kullanicilar[12]);
+
+            foreach(var user in kullanicilar)
+            {
+                Console.WriteLine(user);
+            }
+
+            //Count
+            Console.WriteLine(kullanicilar.Count);
+
+            //Contains
+            Console.WriteLine(kullanicilar.ContainsKey(12));
+            Console.WriteLine(kullanicilar.ContainsValue("Zikriye Ürkmez"));
+
+            //Remove
+            kullanicilar.Remove(12);
+            foreach (var user in kullanicilar)
+            {
+                Console.WriteLine(user);
+            }
+
+            //Keys
+            foreach (var user in kullanicilar.Keys)
+            {
+                Console.WriteLine(user);
+            }
+
+            //Values
+            foreach (var user in kullanicilar.Values)
+            {
+                Console.WriteLine(user);
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
